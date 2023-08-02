e6474bf8a86335c4ee9f6dfb280d81e7e9877572
Inti Luna Aviles <inti.luna.aviles@gmail.com>
2023-08-02 11:23:02 +0000

    add authentication with Flask-JWT-Extended

--- src/app.py
+++ src/app.py
@@ -10,8 +10,20 @@ from utils import APIException, generate_sitemap
 from admin import setup_admin
 from models import db, User, Character, Planet, Favorito
 #from models import Person
+#for authentication
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+from flask_jwt_extended import JWTManager
+#for checking email
+import re
 
 app = Flask(__name__)
+
+# Setup the Flask-JWT-Extended extension
+app.config["JWT_SECRET_KEY"] = "super-secret"  # Change this!
+jwt = JWTManager(app)
+
 app.url_map.strict_slashes = False
 
 db_url = os.getenv("DATABASE_URL")
@@ -67,6 +79,8 @@ def get_one_characters(character_id):
 
     character_query = Character.query.filter_by(id=character_id).first()
     
+    if character_query is None:
+        return jsonify({"msg": "Character not exist"}), 404
 
     response_body = {
        "results": character_query.serialize()
@@ -80,6 +94,9 @@ def get_all_planets():
 
     planets_query = Planet.query.all()
     results = list(map(lambda item: item.serialize(), planets_query))
+
+    if planet_query is None:
+        return jsonify({"msg": "Planet not exist"}), 404
 
     response_body = {
        "results": results
@@ -159,6 +176,9 @@ def add_favorito(user_id):
 # USERS
 
 @app.route('/users', methods=['POST'])
+
+
+
 def create_user():
 
     request_body = request.get_json(force=True)
@@ -167,6 +187,41 @@ def create_user():
                 password=request_body['password'],
                 is_active=request_body['is_active'])
     
+    if request_body['email'] is None or request_body['password'] is None or request_body['is_active'] is None:
+        return jsonify ({
+            'msg':'missing parameters (email, password, is_active are required)'
+        }), 400
+    
+    # Verificamos email válido (basico)
+
+    # if "@" not in request_body['email'] or "." not in request_body['email']:
+    #     return jsonify ({
+    #         'msg':'wrong email format(check @ .)'
+    #     }), 400
+
+    # Verificamos email válido (pro)
+    def validar_email(email):
+        # Patrón de expresión regular para validar el email
+        patron_email = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        
+        # Usamos re.match() para verificar el patrón en el email proporcionado
+        if re.match(patron_email, email):
+            return True
+        else:
+            return False
+
+
+
+    # Ejemplo de uso:
+
+    # email_ejemplo = "usuario@example.com"
+    if validar_email(request_body['email']):
+        print("El email es válido.")
+    else:
+        return jsonify ({
+            'msg':'wrong email format(check @ .)'
+        }), 400
+
     db.session.add(user)
     db.session.commit()
 
@@ -209,6 +264,49 @@ def del_favorito(user_id ):
     }
 
     return jsonify(response_body), 200
+
+
+#add authentication endpoints
+
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@app.route("/login", methods=["POST"])
+def login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    
+    if email == "test" and password == "test":
+        access_token = create_access_token(identity=email)
+        return jsonify(access_token=access_token)
+    
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"msg": "email do not exist"}), 404
+
+    if password != user.password:
+        return jsonify({"msg": "Bad password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+#sample post body to login
+# {
+#     "email": "test",
+#     "password":"test"  
+# }
+
+# Protect a route with jwt_required, which will kick out requests
+# without a valid JWT present.
+@app.route("/profile", methods=["GET"])
+@jwt_required()
+def get_profile():
+    # Access the identity of the current user with get_jwt_identity
+    current_user = get_jwt_identity()
+    user = User.query.filter_by(email=current_user).first()
+    if user is None:
+        return jsonify({"msg": "user do not exist"}), 404
+    return jsonify(logged_in_as=current_user), 200
 
 ########################################### endpoint finished-------------------------------------------------
 
